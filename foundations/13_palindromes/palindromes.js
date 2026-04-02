47bc5398a9290d18954d0343177e687593a3e3bf
Vitudsg <vtvitucosta@gmail.com>
2026-04-02 18:35:51 -0300

    Exercises finish

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let text = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let invertText = text.split('').reverse().join('');
 
+    if (invertText === text) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
