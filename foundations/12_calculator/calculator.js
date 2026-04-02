bb6b4991308e828b6f26640ee284bdc505f22e02
Vitudsg <vtvitucosta@gmail.com>
2026-04-02 18:28:12 -0300

    Exercise finalized

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;   
 };
 
-const sum = function() {
-	
+const sum = function(n) {
+  let total = 0;
+
+	for (let i = 0; i < n.length; i++) {
+    total += n[i];
+  }
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(n) {
+  let total = 1;
 
+  for (let i = 0; i < n.length; i++) {
+    total *= n[i];
+  }
+
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let total = 1;
+
+  for (let i = 0; i < exponent; i ++) {
+    total *= base;
+  }
+  
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let total = 1;
+
+  for (let i = 0; i < n; i ++) {
+    total *= (n - i);
+  }
+
+  return total;
 };
 
 // Do not edit below this line
