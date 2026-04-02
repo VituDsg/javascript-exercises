29a38971f34774a5414670f08c21d94265fb94cb
Vitudsg <vtvitucosta@gmail.com>
2026-04-02 18:54:39 -0300

    Exercises finish

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+    let total = 0;
+    
+    let currentNumber = 1
+    let previousNumber = 0
 
+    if(value < 0) {
+        return "OOPS";
+    }
+
+    for (let i = 0; i < value; i++) {
+        previousNumber = currentNumber;
+        currentNumber = total;
+
+        total = currentNumber + previousNumber;
+    }
+    return total;
 };
 
 // Do not edit below this line
